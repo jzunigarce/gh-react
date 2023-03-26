41ef2fbfce0cedef50dfb3f63013e56194334d15
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2023-03-26 01:21:46 -0700

    Fix path deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://jzunigarce.github.io',
+  base: 'https://jzunigarce.github.io/gh-react/',
   plugins: [react()],
 })
